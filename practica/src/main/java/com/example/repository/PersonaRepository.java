54a75b0b9878b10856134bbb968ea08ba723170c
Francisco <69539055+PerfilFrancisco@users.noreply.github.com>
2023-04-25 14:48:04 -0400

    Delete practica directory
    elimino practica ya que no encuentro que sea necesario seguir teniendola

--- practica/src/main/java/com/example/repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.model.Persona;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long>{
-      
-}
--- practica/src/main/java/com/example/service/api/PersonaServiceAPI.java
+++
@@ -1,8 +0,0 @@
-package com.example.service.api;
-
-import com.example.commons.GenericServiceAPI;
-import com.example.model.Persona;
-
-public interface PersonaServiceAPI extends GenericServiceAPI<Persona, Long>  {
-	
-}
